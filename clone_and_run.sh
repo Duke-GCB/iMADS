b591ef339a246692352448d108ef6909d4277f62
John Bradley <johnbradley2008@gmail.com>
2016-04-29 15:47:44 -0400

    tweak clone and run to pull dev dep so we can build babel correctly

--- clone_and_run.sh
+++ clone_and_run.sh
@@ -2,6 +2,7 @@
 git clone https://github.com/Duke-GCB/TF-DNA-PredictionsDB.git
 cd TF-DNA-PredictionsDB
 npm install
+npm install --dev
 npm install webpack -g
 webpack
 gunicorn --bind 0.0.0.0:80 webserver:app
