f45e9ffbbe333609f6545dce4ee4df49f0a3885b
John Bradley <johnbradley2008@gmail.com>
2016-09-13 08:34:17 -0400

    spacers between model families in dropdown

--- portal/src/app/common/ModelSelect.jsx
+++ portal/src/app/common/ModelSelect.jsx
@@ -5,17 +5,47 @@ import React from 'react';
 import SelectItem from '../common/SelectItem.jsx';
 
 export default class ModelSelect  extends React.Component {
+    /**
+     * Return an option tag based on the mode.
+     * Returns separator if model is undefined.
+     * @param model
+     * @returns {XML}
+     */
     makeOption(model) {
         // Hides numeric portion of names from users.
-        // Additional release should change this dropdown into categories.
-        let userName = model.name.replace(/_.*/, '');
-        return <option key={model.name} value={model.name}>{userName}</option>;
+        // Additional release should change this dropdown into labeled categories.
+        if (model) {
+            let userName = model.name.replace(/_.*/, '');
+            return <option key={model.name} value={model.name}>{userName}</option>;
+        } else {
+            return <option disabled>──────────</option>;
+        }
+    }
+
+    /**
+     * Adds undefined between families so we can create spacers in the dropdown.
+     * @param proteinOptions
+     * @returns {Array}
+     */
+    addSpacers(proteinOptions) {
+        let result = [];
+        if (proteinOptions.length > 0) {
+            let lastFamily = proteinOptions[0].family;
+            for (let model of proteinOptions) {
+                if (model.family != lastFamily) {
+                    result.push(undefined);
+                }
+                result.push(model);
+                lastFamily = model.family;
+            }
+        }
+        return result;
     }
 
     render() {
         let {models, selected, onChange} = this.props;
-
-        let proteinOptions = models.map(this.makeOption);
+        let modelsWithSpacers = [];
+        let proteinOptions = this.addSpacers(models).map(this.makeOption);
         return <SelectItem
             title="Protein/Model:"
             selected={selected}
