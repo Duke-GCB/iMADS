67fb584d7e7ab49ae125c397e37ee8706b8b9eb8
John Bradley <johnbradley2008@gmail.com>
2016-10-24 09:50:04 -0400

    formatModelName comment

--- portal/src/app/models/Model.js
+++ portal/src/app/models/Model.js
@@ -1,4 +1,6 @@
 export function formatModelName(modelName) {
+    // Remove unique model numbers and underscores from the model names.
+    // eg. 'Elk1_0004_vs_Ets1_0005' -> 'Elk1 vs Ets1'
     return modelName.replace(/_[0-9]+/g, '').replace(/_/g, ' ');
 }
 
