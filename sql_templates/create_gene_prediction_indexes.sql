d57102a93e0ad71429f161431f05301c870d173d
John Bradley <johnbradley2008@gmail.com>
2016-10-30 14:08:26 -0400

    remove un-necessary index

--- sql_templates/create_gene_prediction_indexes.sql
+++ sql_templates/create_gene_prediction_indexes.sql
@@ -4,9 +4,6 @@ create index if not exists gene_prediction_grouped_idx
 create index if not exists gene_prediction_value_idx
  on {{ schema_prefix }}.gene_prediction(gene_list, model_name, abs(value) DESC);
 
-create index if not exists gene_prediction_max_sort_idx
- on {{ schema_prefix }}.gene_prediction(gene_list, model_name, common_name);
-
 -- for custom gene list speedup
 create index gene_prediction_common_name_search_idx
  on {{ schema_prefix }}.gene_prediction(upper(common_name));
