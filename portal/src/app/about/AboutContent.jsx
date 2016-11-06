ba24bdae027d7d498ed04d0d3c03a1d8d7ba05d6
John Bradley <johnbradley2008@gmail.com>
2016-11-06 12:23:15 -0500

    Replace dummy text in about tab
    Brief description and links to some source code.

--- portal/src/app/about/AboutContent.jsx
+++ portal/src/app/about/AboutContent.jsx
@@ -2,10 +2,28 @@ import React from 'react';
 require('./AboutContent.css');
 
 class AboutContent extends React.Component {
+    makeAnchor(url) {
+        let urlRef = "https://" + url;
+        return <a href={urlRef}>{url}</a>;
+    }
+
     render() {
+        let websiteSource = this.makeAnchor("github.com/Duke-GCB/iMADS");
+        let predictionSource = this.makeAnchor("github.com/Duke-GCB/Predict-TF-Binding");
+        let preferenceSource = this.makeAnchor("github.com/Duke-GCB/Predict-TF-Preference");
         return <div>
-            <span>Transcription factor website details. TEST</span>
-            
+            <div>
+                iMADS enables searching and creating transcription factor binding predictions and preferences.
+            </div>
+            <br />
+            <div>
+                <span className="AboutContent_source_code_label">Source Code</span>
+                <ul>
+                    <li>Website: {websiteSource}</li>
+                    <li>TF binding predictions: {predictionSource}</li>
+                    <li>TF preference: {preferenceSource}</li>
+                </ul>
+            </div>
         </div>;
     }
 }
