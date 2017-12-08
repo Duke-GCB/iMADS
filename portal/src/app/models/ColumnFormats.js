9ff7c4aae32ecbcccc9849fe00bd85b86250bece
John Bradley <johnbradley2008@gmail.com>
2017-12-08 10:51:11 -0500

    Add download data dialog
    Component to let user choose between download options.
    Adds ColumnFormats that contains the names of various formats and labels.

---
+++ portal/src/app/models/ColumnFormats.js
@@ -0,0 +1,41 @@
+export const GENE_NAMES_WARNING_PREFIX = "Gene names not in our database";
+export const COLUMN_FORMAT_STANDARD = 'standard';
+export const COLUMN_FORMAT_NUMERIC_BINDING_SITES = 'numericBindingSites';
+export const COLUMN_FORMAT_BINDING_SITES_LIST = 'bindingSiteList';
+export const COLUMN_FORMAT_RAW_DATA = 'rawData';
+
+export function GeneNameColumnFormats() {
+    const standardLabel = 'Name, ID, Max iMADS score, Chromosome, Start, End';
+    const numericBindingSitesLabel = standardLabel + ', followed by all values';
+    const bindingSiteListLabel = 'Name, ID, Binding site location, Binding site score, Binding site sequence ' +
+        '(one line per binding site)';
+    const formats = {};
+    formats[COLUMN_FORMAT_STANDARD] = { label: standardLabel };
+    formats[COLUMN_FORMAT_NUMERIC_BINDING_SITES] = { label: numericBindingSitesLabel };
+    formats[COLUMN_FORMAT_BINDING_SITES_LIST] = { label: bindingSiteListLabel };
+    return formats;
+}
+
+export function CustomRangeColumnFormats() {
+    const standardLabel = 'Chromosome, Start, End, Max iMADS score';
+    const numericBindingSitesLabel = standardLabel + ', followed by all values';
+    const bindingSiteListLabel = 'Chromosome, Start, End, ' +
+        'Binding site location, Binding site score, Binding site sequence ' +
+        '(one line per binding site)';
+    const formats = {};
+    formats[COLUMN_FORMAT_STANDARD] = { label: standardLabel };
+    formats[COLUMN_FORMAT_NUMERIC_BINDING_SITES] = { label: numericBindingSitesLabel };
+    formats[COLUMN_FORMAT_BINDING_SITES_LIST] = { label: bindingSiteListLabel };
+    return formats;
+}
+
+export function CustomDNAColumnFormats() {
+    const standardLabel = 'Name, Sequence, Max iMADS score';
+    const numericBindingSitesLabel = standardLabel + ', followed by all values';
+    const rawFormatLabel = 'Name, Start, Stop, iMADS score (BED Format)';
+    const formats = {};
+    formats[COLUMN_FORMAT_STANDARD] = { label: standardLabel };
+    formats[COLUMN_FORMAT_NUMERIC_BINDING_SITES] = { label: numericBindingSitesLabel };
+    formats[COLUMN_FORMAT_RAW_DATA] = { label: rawFormatLabel, isTabDelimited: true};
+    return formats;
+}
