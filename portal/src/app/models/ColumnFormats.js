f0d2164c2d97b00c9351c427f2128b5176e09cbb
John Bradley <johnbradley2008@gmail.com>
2018-03-05 13:27:27 -0500

    update download data format labels
    Update UI with new labels based on #143
    Still need to update csv/tsv/bed file generation code.

--- portal/src/app/models/ColumnFormats.js
+++ portal/src/app/models/ColumnFormats.js
@@ -4,10 +4,12 @@ export const COLUMN_FORMAT_BINDING_SITES_LIST = 'bindingSiteList';
 export const COLUMN_FORMAT_RAW_DATA = 'rawData';
 
 export function GeneNameColumnFormats() {
-    const standardLabel = 'Name, ID, Max iMADS score, Chromosome, Start, End';
-    const numericBindingSitesLabel = standardLabel + ', followed by all values';
-    const bindingSiteListLabel = 'Name, ID, Binding site location, Binding site score, Binding site sequence ' +
-        '(one line per binding site)';
+    const standardLabel = 'Name, ID, Genomic region coordinates, Maximum iMADS score ' +
+        '(One line per region)';
+    const numericBindingSitesLabel = 'Name, ID, Genomic region coordinates, iMADS scores for all positions in region ' +
+        '(One line per region)';
+    const bindingSiteListLabel = 'Name, ID, Binding site location, Binding site iMADS score, Binding site sequence ' +
+        '(One line per site)';
     const formats = {};
     formats[COLUMN_FORMAT_STANDARD] = { label: standardLabel };
     formats[COLUMN_FORMAT_NUMERIC_BINDING_SITES] = { label: numericBindingSitesLabel };
@@ -16,11 +18,10 @@ export function GeneNameColumnFormats() {
 }
 
 export function CustomRangeColumnFormats() {
-    const standardLabel = 'Chromosome, Start, End, Max iMADS score';
-    const numericBindingSitesLabel = standardLabel + ', followed by all values';
-    const bindingSiteListLabel = 'Chromosome, Start, End, ' +
-        'Binding site location, Binding site score, Binding site sequence ' +
-        '(one line per binding site)';
+    const standardLabel = 'Genomic region coordinates, Maximum iMADS score (One line per region)';
+    const numericBindingSitesLabel = 'Genomic region coordinates, iMADS scores for all positions in region (One line per region)';
+    const bindingSiteListLabel = 'Genomic region coordinates, Binding site coordinates, ' +
+        'Binding site iMADS score, Binding site >sequence (One line per site)';
     const formats = {};
     formats[COLUMN_FORMAT_STANDARD] = { label: standardLabel };
     formats[COLUMN_FORMAT_NUMERIC_BINDING_SITES] = { label: numericBindingSitesLabel };
@@ -29,9 +30,9 @@ export function CustomRangeColumnFormats() {
 }
 
 export function CustomDNAColumnFormats() {
-    const standardLabel = 'Name, Sequence, Max iMADS score';
-    const numericBindingSitesLabel = standardLabel + ', followed by all values';
-    const rawFormatLabel = 'Name, Start, Stop, iMADS score (BED Format)';
+    const standardLabel = 'Name, Sequence, Maximum iMADS score (One line per input sequence)';
+    const numericBindingSitesLabel = 'Name, Sequence, iMADS scores for all positions (One line per input sequence)';
+    const rawFormatLabel = 'Name, Start, Stop, iMADS score, Binding site sequence (BED format; one line per binding site)';
     const formats = {};
     formats[COLUMN_FORMAT_STANDARD] = { label: standardLabel };
     formats[COLUMN_FORMAT_NUMERIC_BINDING_SITES] = { label: numericBindingSitesLabel };
